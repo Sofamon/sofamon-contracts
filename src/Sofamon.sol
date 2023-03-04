1c5bc3e1eb7d8b18c70dfe8fd75f1ec48b7afc85
XingyuLi20 <lixingyu@berkeley.edu>
2023-03-04 15:42:34 -0700

    add: sofamon polygon

---
+++ src/Sofamon.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "solmate/tokens/ERC1155.sol";
+import "solmate/utils/LibString.sol";
+
+error NotEnoughETH();
+
+contract Sofamon is ERC1155, Ownable {
+    uint256 public constant PRICE_PER_MINT = 0 ether;
+
+    string public baseMetadataURI;
+
+    // mapping(bytes => bool) public signatureUsed;
+
+    constructor() ERC1155() {}
+
+    function setURI(string memory baseMetadataURI_) public onlyOwner {
+        baseMetadataURI = baseMetadataURI_;
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        return strConcat(baseMetadataURI, LibString.toString(id));
+    }
+
+    function strConcat(string memory _a, string memory _b)
+        internal
+        pure
+        returns (string memory)
+    {
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        string memory ab = new string(_ba.length + _bb.length);
+        bytes memory bab = bytes(ab);
+        uint256 k = 0;
+        for (uint256 i = 0; i < _ba.length; i++) bab[k++] = _ba[i];
+        for (uint256 i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
+        return string(bab);
+    }
+
+    function mint(
+        uint256 tokenId,
+        uint256 amount,
+        address to
+    ) external payable {
+        if (msg.value < amount * PRICE_PER_MINT) revert NotEnoughETH();
+        _mint(to, tokenId, amount, "");
+    }
+
+    function withdraw() public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
---
+++ test/Sofamon.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+/*
+███████╗ ██████╗ ███████╗ █████╗ ███╗   ███╗ ██████╗ ███╗   ██╗
+██╔════╝██╔═══██╗██╔════╝██╔══██╗████╗ ████║██╔═══██╗████╗  ██║
+███████╗██║   ██║█████╗  ███████║██╔████╔██║██║   ██║██╔██╗ ██║
+╚════██║██║   ██║██╔══╝  ██╔══██║██║╚██╔╝██║██║   ██║██║╚██╗██║
+███████║╚██████╔╝██║     ██║  ██║██║ ╚═╝ ██║╚██████╔╝██║ ╚████║
+╚══════╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝
+*/
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Sofamon.sol";
+
+contract SofamonTest is Test {
+    using stdStorage for StdStorage;
+
+    Sofamon private nft;
+
+    function setUp() public {
+        // Deploy NFT contract
+        nft = new Sofamon();
+    }
+}
